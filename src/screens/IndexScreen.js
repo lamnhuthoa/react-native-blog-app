991b44cd035eeb541c5f6c866c87c1c0c8f42eae
lamnhuthoa <hoalamnhut@gmail.com>
2023-05-03 21:17:07 +0700

    Blog post init v1.0

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import BlogContext from "../context/BlogContext";
+
+const IndexScreen = () => {
+const {data, addBlogPost} = useContext(BlogContext);
+
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList 
+        data={data}
+        keyExtractor={(blogPosts) => blogPosts.title}
+        renderItem={({item}) => {
+            return <Text>{item.title}</Text>
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default IndexScreen;
